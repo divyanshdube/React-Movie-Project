85a0bff484626436863d604aee7dd531ef2f7bcf
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-12 12:12:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,13 +13,17 @@ const App = () => {
   const [searchValue, setSearchValue] = useState("");
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    try {
+      const url = `HTTPS://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
+      const response = await fetch(url);
+      const responseJson = await response.json();
 
-    if (responseJson.Search) {
-      setMovies(responseJson.Search);
+      if (responseJson.Search) {
+        setMovies(responseJson.Search);
+      }
+    } catch {
+      console.log("Error");
     }
   };
 
